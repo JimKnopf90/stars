410b0df2131ffc79397a3da725a46832c734d797
CapslockJesus <Daniel.Reichert.1991@gmx.de>
2018-03-07 21:01:06 +0100

    icon geändert

--- sites/login.php
+++ sites/login.php
@@ -11,7 +11,7 @@ $_SESSION = array();
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 		<link rel="stylesheet" type="text/css" href="../css/style-login.css">
-		<link rel="icon" href="../image/icon.jpg">
+		<link rel="icon" href="../image/icon-hurensohn.jpg">
 		<title>Login</title>
 	</head>
 	<body>
