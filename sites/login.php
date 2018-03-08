afb460aacebf97ce9ae20394c96ef3e39a833ab5
OBERFLÄCHLICH\Timm <t.reichert90@outlook.de>
2018-03-08 18:10:44 +0100

    Bild

--- sites/login.php
+++ sites/login.php
@@ -12,30 +12,24 @@ $_SESSION = array();
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 		<link rel="stylesheet" type="text/css" href="../css/style-login.css">
 		<link rel="icon" href="../image/icon.jpg">
-		<title>Login</title>
+		<title>StaRs | Login</title>
 	</head>
 	<body>
-		<div class="container">
-		
-			<form action="../business-logic/mLogin.php" method="post">		
+        <div class="container">
+            <img src="../image/icon-big-user.png">
+            
+            <form action="../business-logic/mLogin.php" method="post">
                 <div id="form-input-user">
-					<img title="Benutzernamen eingeben" class="login-icon" src="../image/icon-benutzername.png">
-					<input type="text" name="username" placeholder="Benutzername">
-				</div>
-				<div id="form-input-pass">
-					<img title="Passwort eingeben" class="login-icon" src="../image/icon-schloss.png">
-					<input type="password" name="password" placeholder="Passwort">
-				</div>
-				<div class="button">
-                    <button id="btnSubmit" type="submit" name="submit" value="Einloggen">Einloggen</button>
-				</div>
-				<a href="forget-password.php"> Passwort vergessen </a>
-				<a href="contact.php"> Kontaktieren </a>
-				<div id="logo">
-					<img class="logo-logo" src="../image/verpacking-logo.png">
-				</div>	
-			</form>
-		</div>
-	
-	</body>
+                    <input type="text" name="username" placeholder="Benutzernamen">
+                </div>
+                
+                <div class="form-input">
+                    <input type="password" name="password" placeholder="Passwort">
+                </div>
+                
+                <input type="submit" name="submit" value="Einloggen">
+                
+            </form>
+        </div>    
+    </body>
 </html>
