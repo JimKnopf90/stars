560fb831078aa004a0bafdaebbddbf73b69c2697
CapslockJesus <Daniel.Reichert.1991@gmx.de>
2018-03-14 16:41:00 +0100

    Admin Design angepasst

--- sites/admin-login.php
+++ sites/admin-login.php
@@ -3,8 +3,8 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-		<link rel="stylesheet" type="text/css" href="../css/admin-stylesheet.css">
-		<link rel="icon" href="../image/icon.jpg">
+		<link rel="stylesheet" type="text/css" href="../css/admin-login-stylesheet.css">
+		<link rel="icon" href="../admin-image/stars-logo.png">
 		<script type="text/javascript">
 		</script>
 		<title>StaRs | Adminbereich</title>
@@ -12,8 +12,8 @@
 	<body>
         <img class="stars-logo" src="../admin-image/stars-logo.png">
         <div class="login">
-            <h1>Login</h1>
-            <form method="post" action="#">
+            <!-- <h1>Login</h1> -->
+            <form method="post" action="admin-startseite.php">
                 <input type="text" id="username" name="user" placeholder="Benutzername" required="required">
                 <input type="password" id="password" name="password" placeholder="Passwort" required="required">
                 <input type="submit" id="btn-login" value="Login">
---
+++ sites/admin-startseite.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<link rel="stylesheet" type="text/css" href="../css/admin-stylesheet.css">
+		<link rel="icon" href="../admin-image/stars-logo.png">
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jequery.min.js">
+        </script>
+		<title>StaRs | Adminbereich</title>
+	</head>
+	<body>
+        <!-- <header>
+            <div id="header-button">
+            <ul>
+                    <li><img class="stars-logo" src="../admin-image/stars-logo.png"></li>
+                    <li>
+                        <button type="button" class="active">
+                            User Bearbeiten
+                        </button>
+                    </li>
+                    <li>
+                        <button type="button" class="header-menu">
+                            Rechte verwalten
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </header> -->
+        <header>
+            <img id="stars-logo" src="../admin-image/stars-logo.png" />
+        </header>
+        <div class="sidebar">
+            <ul>                
+                <li>
+                    <a class="btnSidebar" href="#">
+                        <button type="button" class="active">
+                            Dashboard
+                        </button>
+                    </a>
+                </li>
+                
+                <li>
+                    <a class="btnSidebar" href="#">
+                        <button type="button" class="inactive">
+                            Benutzer
+                        </button>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="btnSidebar" href="#">
+                        <button type="button" class="inactive">
+                            Versandklassen
+                        </button>
+                    </a>
+                </li>
+                
+                <li>
+                    <a class="btnSidebar" href="#">
+                        <button type="button" class="inactive">
+                            Export
+                        </button>
+                    </a>
+                </li>
+                
+                <li>
+                    <a class="btnSidebar" href="#">
+                        <button type="button" class="inactive">
+                            Newsfeed
+                        </button>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="btnSidebar" href="#">
+                        <button type="button" class="inactive">
+                            Sonstiges
+                        </button>
+                    </a>
+                </li>
+                
+                <li>
+                    <a class="btnSidebar" href="#">
+                        <button type="button" class="inactive">
+                            Design
+                        </button>
+                    </a>
+                </li>
+            </ul>
+        </div>  
+        
+        <!-- <img class="btn" src="../admin-image/homebutton.png" /> -->
+        
+    </body>
+</html>
