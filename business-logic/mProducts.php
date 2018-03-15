7b630571cd267412b4b781645a381a7cd54c6e3b
OBERFLÄCHLICH\Timm <t.reichert90@outlook.de>
2018-03-15 20:55:14 +0100

    Bisschen Suche und Style änderung

--- business-logic/mProducts.php
+++ business-logic/mProducts.php
@@ -3,10 +3,11 @@
     include("mConErp.php");   
     
     
+    
     $dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname","$dbusername","$pw");
     
     //Paging
-    $limit = 20;
+    $limit = 100;
     
     
     if (isset($_GET["page"])) { 
@@ -39,24 +40,25 @@ ORDER BY AVAL.Bezeichnung, AVAL.kArtikel OFFSET $start_from ROWS FETCH NEXT $lim
    
         //Table beginn
         echo "<table>";
-        
+        echo "<form>";
         // Headline
+        
         echo "<tr><th><a href='login.php'><img class='icon-art-settings' src='../image/icon-art-setting.png' /></a></th>";
-        echo "<th id='th-plattform'>Plattform</th>";
-        echo "<th id='th-artikelnummer'>Artikelnummer</th>";
-        echo "<th id='th-artikelname'>Artikelname</th>";
-        echo "<th id='th-hersteller'>Hersteller</th>";
-        echo "<th id='th-plattform-id'>Plattform-ID</th>";
-        echo "<th id='th-ek-netto'>EK-Netto</th>";
-        echo "<th id='th-mehrwertsteuer'>Mwst.</th>";
-        echo "<th id='th-versandklasse'>Versandklasse</th>";
-        echo "<th id='th-gewicht'>Gewicht</th>";
-        echo "<th id='th-nullpreis'>Nullpreis</th>";
-        echo "<th id='th-vk-preis'>VK Preis</th>";
-        echo "<th id='th-marge-euro'>Marge €</th>";
-        echo "<th id='th-marge-prozent'>Marge %</th>";
-        echo "<th id='th-bestand'>Bestand</th>";
-        echo "<th id='th-ordner'>Ordner</th></tr>";
+        echo "<th id='th-plattform'>Plattform <br><input id='txt-plattform'></th>";
+        echo "<th id='th-artikelnummer'>Artikelnummer <br><input id='txt-artikelnummer'></th>";
+        echo "<th id='th-artikelname'>Artikelname <br><input id='txt-artikelname'></th>";
+        echo "<th id='th-hersteller'>Hersteller <br><input id='txt-hersteller'></th>";
+        echo "<th id='th-plattform-id'>Plattform-ID <br><input id='txt-plattformid'></th>";
+        echo "<th id='th-ek-netto'>EK-Netto <br><input id='txt-eknetto'></th>";
+        echo "<th id='th-mehrwertsteuer'>MwSt. <br><input id='txt-mwst'></th>";
+        echo "<th id='th-versandklasse'>Versandklasse <br><input id='txt-versandklasse'></th>";
+        echo "<th id='th-gewicht'>Gewicht <br><input id='txt-gewicht'></th>";
+        echo "<th id='th-nullpreis'>Nullpreis <br><input id='txt-nullpreis'></th>";
+        echo "<th id='th-vk-preis'>VK Preis <br><input id='txt-vkpreis'></th>";
+        echo "<th id='th-marge-euro'>Marge € <br><input id='txt-margeeuro'></th>";
+        echo "<th id='th-marge-prozent'>Marge % <br><input id='txt-margeprozent'></th>";
+        echo "<th id='th-bestand'>Bestand <br><input id='txt-bestand'></th>";
+        echo "<th id='th-ordner'>Ordner <br><input id='txt-ordner'></th></tr>";
         
         
         foreach ($dbh->query($sql) as $row) {
@@ -66,13 +68,12 @@ ORDER BY AVAL.Bezeichnung, AVAL.kArtikel OFFSET $start_from ROWS FETCH NEXT $lim
             echo "<td id='td-plattform'>" .$row["Plattform"] . "</td>";
             echo "<td id='td-artikelnummer'>" .$row["Artikelnummer"] . "</td>";
             echo "<td id='td-artikelname'>" .$row["Bezeichnung"] . "</td>";
-            // echo "<td id='td-hersteller'>" .$row["Hersteller"] . "</td>";
             echo "<td id='td-hersteller'> Brennenstuhl </td>";
             echo "<td id='td-plattform-id'>" .$row["ASIN"] . "</td>";
-            echo "<td id='td-ek-netto'>" . floatval($row["GesamtEkNetto"]) . "</td>";
-            echo "<td id='td-mehrwertsteuer'>" . floatval($row["fSteuersatz"]) . "</td>";
+            echo "<td id='td-ek-netto'>" . number_format(floatval($row["GesamtEkNetto"]),2, ",", ".") . " &#8364</td>";
+            echo "<td id='td-mehrwertsteuer'>" . floatval($row["fSteuersatz"]) . " %</td>";
             echo "<td id='td-versandklasse'>" .$row["cName"] . "</td>";
-            echo "<td id='td-gewicht'>" . floatval($row["Versandgewicht"]) . "</td>";
+            echo "<td id='td-gewicht'>" . number_format(floatval($row["Versandgewicht"]),2, ",", ".") . "</td>";
             echo "<td id='td-nullpreis'> 14,44 €</td>";
             echo "<td id='td-vk-preis'> 29,95 €</td>";
             echo "<td id='td-marge-euro'> 1,22 €</td>";
@@ -83,6 +84,7 @@ ORDER BY AVAL.Bezeichnung, AVAL.kArtikel OFFSET $start_from ROWS FETCH NEXT $lim
         }
         
         //Table conclusion
+        echo "</form>";
         echo "</table>";
         
         $sql = "SELECT DISTINCT COUNT(Artikelnummer)
@@ -96,10 +98,10 @@ ORDER BY AVAL.Bezeichnung, AVAL.kArtikel OFFSET $start_from ROWS FETCH NEXT $lim
         $total_records = $count->fetchColumn();
         $total_pages = ceil($total_records / $limit);  
         
-        $pagLink = "<div class='pagination'>";
+        $pagLink = "<div id='paging' class='pagination'>";
         
         for ($i=1; $i<=$total_pages; $i++) {
-            $pagLink .= "<a href='products.php?page=".$i."'>".$i."</a>";
+            $pagLink .= "<a class='pagingElement' href='products.php?page=".$i."'>".$i."</a>";
         };
         echo $pagLink . "</div>";  
 
