80f15203327a4e6fa8391eca74f925d4e72e2b2c
CapslockJesus <Daniel.Reichert.1991@gmx.de>
2018-05-24 19:17:00 +0200

    css datei angepasst

--- sites/admin-lagerbestand.php
+++ sites/admin-lagerbestand.php
@@ -8,7 +8,7 @@ if(isset($_SESSION["adminusername"])) {
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 		<link rel="stylesheet" type="text/css" href="../css/admin-stylesheet.css">
-		<link rel="stylesheet" type="text/css" href="../css/admin-lagerbestand-stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="../css/admin-table-stylesheet.css">
 		<link rel="icon" href="../admin-image/stars-logo.png">
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jequery.min.js">
         </script>
@@ -121,7 +121,7 @@ if(isset($_SESSION["adminusername"])) {
             </ul>
         </div>  
         <div class="content">
-            <table>
+            <table id="lb-table">
                 <tr id="lb-table-header">
                     <th>Kategorie</th>
                     <th>Artikelnummer</th>
