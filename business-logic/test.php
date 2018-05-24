21081cdbb2a6f0b6810d68ee22afa2fa0e41f0a0
CapslockJesus <Daniel.Reichert.1991@gmx.de>
2018-05-24 21:36:14 +0200

    PHP geübt. Testseiten könnten entfernt werden, werden aber noch gebraucht.

---
+++ business-logic/test.php
@@ -0,0 +1,12 @@
+<?php
+    echo '<table id="lb-table"><tr id="lb-table-header">';
+    echo '<th>Kategorie</th>';
+    echo '<th>Art.Nr.</th>';
+    echo '<th>Art.Name</th>';
+    echo '<th>ges. Lagerbestand</th>';
+    echo '<th>in Aufträgen</th>';
+    echo '<th>verf. Lagerbestand</th>';
+    echo '<th>Bestandswert</th></tr>';
+    echo '<tr><td>';
+    echo '</table>';
+?>
